019d7b59e422bd5600e0e1ba6fd77f12193a428d
Sarthak Mittal <sarthakmittal20@gmail.com>
2016-01-26 01:34:22 +0530

    add major changes and ignore debugging statements

--- maze.c
+++ maze.c
@@ -1,17 +1,15 @@
 #include<stdio.h>
-# define MAX 13
+# define MAX 9
  
-void wall_off();
 void print_maze();
 void making_maze();
-//void set_status();
 void move_fwd(int x, int y);
 
 struct node{
 char img;
 int pre_x;
 int pre_y;
-int status[4];
+int status; // status = 1 is occupied and 0 is empty.
 }arr[MAX][MAX];
 
 
@@ -19,106 +17,32 @@ int main()
 {
 int i,j;
 making_maze();
-wall_off();
-move_fwd(1,1);
+arr[2][2].img = '.';
+arr[2][2].status = 1;
+move_fwd(2,2);
 print_maze();
-//set_status();
-
-/*	for(i = 1 ; i<MAX-1 ; i++)
-	{
-		for(j = 1 ; j<MAX-1 ; j++)
-			printf("%d.%d.%d.%d---",arr[i][j].status[0],arr[i][j].status[1],arr[i][j].status[2],arr[i][j].status[3]);
-		printf("\n\n\n");
-	}
-*/
 
 
 }
-/*
-void set_status()
-{
-int i,j;
-
-	for(i = 1 ; i<MAX-1 ; i=i+1)
-	{
-		for(j = 1 ; j<MAX-1 ; j=j+1)
-		{
-//status for walled
-		   if(arr[i-1][j].img == '#')
-			arr[i][j].status[0] = 8;
-		   if(arr[i][j+1].img == '#')
-			arr[i][j].status[1] = 8;
-		   if(arr[i+1][j].img == '#')
-			arr[i][j].status[2] = 8;
-		   if(arr[i][j-1].img == '#')
-			arr[i][j].status[3] = 8;
-//status for undecided
-		   if(arr[i-1][j].img == '*')
-			arr[i][j].status[0] = 3;
-		   if(arr[i][j+1].img == '*')
-			arr[i][j].status[1] = 3;
-		   if(arr[i+1][j].img == '*')
-			arr[i][j].status[2] = 3;
-		   if(arr[i][j-1].img == '*')
-			arr[i][j].status[3] = 3;
-//status for path
-		   if(arr[i-1][j].img == '.')
-			arr[i][j].status[0] = 1;
-		   if(arr[i][j+1].img == '.')
-			arr[i][j].status[1] = 1;
-		   if(arr[i+1][j].img == '.')
-			arr[i][j].status[2] = 1;
-		   if(arr[i][j-1].img == '.')
-			arr[i][j].status[3] = 1;
-				
-		}	
-
-	}
-
-}
-
-
-// walled => 8
-// unddecided => 3
-// pathed => 1
-//  * 1 *
-//  4 - 2 
-//  * 3 *
-
-*/
 void print_maze()
 {
 int i,j;
 
-	for(i = 0 ; i<MAX ; i++)
+	for(i = 1 ; i<MAX-1 ; i++)
 	{
-		for(j = 0 ; j<MAX ; j++)
+		for(j = 1 ; j<MAX-1 ; j++)
 			printf("%c ",arr[i][j].img);
 		printf("\n");
 	}
+printf("\n..................................................\n");
+	
 
-}
-
-
-void wall_off()
-{
-int i,j;
-
-	i = 0;
-	for(j = 0 ; j<MAX ; j++)
-		arr[i][j].img = '#';
-
-	i = MAX - 1;
-	for(j = 0 ; j<MAX ; j++)
-		arr[i][j].img = '#';
-
-	j = 0;
-	for(i = 0 ; i<MAX ; i++)
-		arr[i][j].img = '#';
-
-	j = MAX - 1;
-	for(i = 0 ; i<MAX ; i++)
-		arr[i][j].img = '#';
+	for(i = 1 ; i<MAX-1 ; i++)
+	{
+		for(j = 1 ; j<MAX-1 ; j++)
+			printf("%d ",arr[i][j].status);
+		printf("\n");
+	}
 
 }
 
@@ -127,23 +51,60 @@ void making_maze()
 {
 int i,j;
 
-//filling everything | '*' will represent undecided 
+//filling blocks which will always be walls : '#'
 for(i = 0 ; i<MAX ; i++)
 {	for(j = 0 ; j<MAX ; j++)
-		arr[i][j].img = '*';
-}
-
-//filling blocks which will always be walls : '#'
-for(i = 0 ; i<MAX ; i=i+2)
-{	for(j = 0 ; j<MAX ; j=j+2)
 		arr[i][j].img = '#';
 }
 
-//filling blocks which will always be path : '.'
-for(i = 1 ; i<MAX ; i=i+2)
-{	for(j = 1 ; j<MAX ; j=j+2)
-		arr[i][j].img = '.';
-}
+//following lines will mark status as occupied for boundary walls
+	i = 0;
+	for(j = 0 ; j<MAX ; j++)
+	{	
+		arr[i][j].status = 1;
+	}
+
+	i = MAX - 1;
+	for(j = 0 ; j<MAX ; j++)
+	{	
+		arr[i][j].status = 1;
+	}
+
+	j = 0;
+	for(i = 0 ; i<MAX ; i++)
+	{	
+		arr[i][j].status = 1;
+	}
+
+	j = MAX - 1;
+	for(i = 0 ; i<MAX ; i++)
+	{	
+ 		arr[i][j].status = 1;
+  	}
+///////////////////////////////
+i = 1;
+	for(j = 1 ; j<MAX-1 ; j++)
+	{	
+		arr[i][j].status = 1;
+	}
+
+	i = MAX - 2;
+	for(j = 1 ; j<MAX-1 ; j++)
+	{	
+		arr[i][j].status = 1;
+	}
+
+	j = 1;
+	for(i = 1 ; i<MAX-1 ; i++)
+	{	
+		arr[i][j].status = 1;
+	}
+
+	j = MAX - 2;
+	for(i = 1 ; i<MAX-1 ; i++)
+	{	
+ 		arr[i][j].status = 1;
+  	}
 
 }
 
@@ -152,52 +113,103 @@ void move_fwd(int x, int y)
 // here random array function will be called
 // obtained array will be used in loop.
 
-int rand_arr[4];
+int rand_arr[4];// this array gives the direction to our current location.
+		/*
+		* 1 = top
+		* 2 = right
+		* 3 = bottom
+		* 4 = left
+		*/
+
 int i;
 
-for(i=0 ; i<4 ; i++)
-rand_arr[i] = i+1;
+//for(i=0 ; i<4 ; i++)
+//rand_arr[i] = i+1;
 
-for(i=0 ; i<4 ; i++)
+for(i=4 ; i<5 ; i++)
 {
-switch(rand_arr[i])
+switch(i)
 {
 
 case 1:
 
-	if(arr[x+1][y].img == '*')
+	if(arr[x+2][y].status == 0)
 	{
-	arr[x+1][y].pre_x = x;
-	arr[x+1][y].pre_y = y;
+	arr[x+2][y].pre_x = x;
+	arr[x+2][y].pre_y = y;
 	arr[x+1][y].img = '.';
-	move_fwd(x+1,y);
+	arr[x+2][y].img = '.';
+	arr[x+2][y].status = 1;
+	arr[x+1][y].status = 1;
+print_maze();
+
+	move_fwd(x+2,y);
+	}
+	else if(arr[x+2][y].status == 1)
+	{
+print_maze();
+
+	move_fwd(arr[x][y].pre_x,arr[x][y].pre_y);
 	}
 	break;
 case 2:
-	if(arr[x-1][y].img == '*')
+	if(arr[x-2][y].status == 0)
 	{
-	arr[x-1][y].pre_x = x;
-	arr[x-1][y].pre_y = y;
+	arr[x-2][y].pre_x = x;
+	arr[x-2][y].pre_y = y;
 	arr[x-1][y].img = '.';
-	move_fwd(x-1,y);
+	arr[x-2][y].img = '.';
+	arr[x-2][y].status = 1;
+	arr[x-1][y].status = 1;
+print_maze();
+
+	move_fwd(x-2,y);
+	}
+	else if(arr[x-2][y].status == 1)
+	{
+print_maze();
+
+	move_fwd(arr[x][y].pre_x,arr[x][y].pre_y);
 	}
 	break;
 case 3:
-	if(arr[x][y+1].img == '*')
+	if(arr[x][y+2].status == 0)
 	{
-	arr[x][y+1].pre_x = x;
-	arr[x][y+1].pre_y = y;
+	arr[x][y+2].pre_x = x;
+	arr[x][y+2].pre_y = y;
 	arr[x][y+1].img = '.';
-	move_fwd(x,y+1);
+	arr[x][y+2].img = '.';
+	arr[x][y+2].status = 1;
+	arr[x][y+1].status = 1;
+print_maze();
+
+	move_fwd(x,y+2);
+	}
+	else if(arr[x][y+2].status == 1)
+	{
+print_maze();
+
+	move_fwd(arr[x][y].pre_x,arr[x][y].pre_y);
 	}
 	break;
 case 4:
-	if(arr[x][y-1].img == '*')
+	if(arr[x][y-2].status == 0)
 	{
-	arr[x][y-1].pre_x = x;
-	arr[x][y-1].pre_y = y;
+	arr[x][y-2].pre_x = x;
+	arr[x][y-2].pre_y = y;
 	arr[x][y-1].img = '.';
-	move_fwd(x,y-1);
+	arr[x][y-2].img = '.';
+	arr[x][y-2].status = 1;
+	arr[x][y-1].status = 1;
+print_maze();
+
+	move_fwd(x,y-2);
+	}
+	else if(arr[x][y-2].status == 1)
+	{
+print_maze();
+
+	move_fwd(arr[x][y].pre_x,arr[x][y].pre_y);
 	}
 	break;
 
